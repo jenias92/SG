b38b143c86b5df3943007d79f0e842ec658d1927
yuval rozilio <yuval291195@gmail.com>
2020-07-08 20:26:31 +0300

    added new files

---
+++ ImportFiles/src/StatusMatch/StatusMatchController.java
@@ -0,0 +1,93 @@
+package StatusMatch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Mitnadev.MitnadevModel;
+import Mitnadev.MitnadevPage;
+import Users.User;
+import Volunteer.Volunteers;
+import Elders.*;
+public class StatusMatchController {
+	StatusMatchView view;
+	StatusMatchModel model;
+	Volunteers VolunteerData;
+
+	public StatusMatchController(StatusMatchView v, StatusMatchModel m) throws IOException {
+		this.view = v;
+		this.model = m;
+
+	}
+	public void Init(Volunteers vol) {
+	this.VolunteerData=vol;
+	if(vol.getMatchStatus().equals("null"))
+	{
+		TurnOffButtons();
+		view.getFindmatch().addActionListener(e->{
+			
+			try {
+				List<Elders> listElder = Elders.ReadDataFromDB();
+				for (int i = 0; i < listElder.size(); i++) {
+				 if(VolunteerData.getMatchStatus().equals("null"))
+				  {
+					if(listElder.get(i).getCity().equals(VolunteerData.getCity()))
+					{
+						VolunteerData.setMatchStatus(listElder.get(i).getId());
+						 listElder.get(i).setMatchId(VolunteerData.GetId());
+						 VolunteerData.InsertIntoDB(VolunteerData);
+						 System.out.print(true);
+						 showMatchedView();
+				    } 
+				  }
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+		}
+	else{
+		showMatchedView();
+	}
+	}
+	public void showMatchedView(){
+		view.getFindmatch().setVisible(false);
+		view.getAddress().setText("יאמייאמי");
+		List<Elders> listElder;
+		try {
+			listElder = Elders.ReadDataFromDB();
+			for (int i = 0; i < listElder.size(); i++) {
+				if(listElder.get(i).getId().equals(VolunteerData.getMatchStatus()))
+				{
+					view.getAddressAnswer().setText(listElder.get(i).getAddress());
+					view.getBirthdayAnswer().setText(listElder.get(i).getBirthDay());
+					view.getCityAnswer().setText(listElder.get(i).getCity());
+					view.getFirstnameAnswer().setText(listElder.get(i).getFirstName());
+					view.getHobbiesAnswer().setText(listElder.get(i).getHobbies());
+					view.getIdAnswer().setText(listElder.get(i).getId());
+					view.getLanguagesAnswer().setText(listElder.get(i).getLanguages());
+					view.getLastnameAnswer().setText(listElder.get(i).getLastName());
+					view.getTelephoneAnswer().setText(listElder.get(i).getTelephone());	
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+		
+	}
+	public void TurnOffButtons() {//מסתיר את פרטי הקשיש
+		view.getAddress().setVisible(false);
+		view.getBirthDay().setVisible(false);
+		view.getCity().setVisible(false);
+		view.getFirstname().setVisible(false);
+		view.getHobbies().setVisible(false);
+		view.getId().setVisible(false);
+		view.getLanguages().setVisible(false);
+		view.getLastname().setVisible(false);
+		view.getTelephone().setVisible(false);
+	}
+}
